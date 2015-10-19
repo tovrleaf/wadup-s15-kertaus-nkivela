b6bed480a3bf57a252416ae005fb0968e0cde3b8
Niko Kivelä <niko.kivela@verkkokauppa.com>
2015-10-20 00:04:19 +0300

    Initial commit
    Copy from first exercise from first week

---
+++ src/main/java/wad/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package wad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloWorldController {
+    @RequestMapping("*")
+    public String hello() {
+        return "Hello World!";
+    }
+}

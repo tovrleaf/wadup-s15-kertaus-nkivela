54ccd12572188a9b02a26a6b885a2156c1eaadbf
Niko Kivelä <niko.kivela@verkkokauppa.com>
2015-10-21 01:01:55 +0300

    Remove ununsed code

--- src/main/java/wad/domain/Exam.java
+++ src/main/java/wad/domain/Exam.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;

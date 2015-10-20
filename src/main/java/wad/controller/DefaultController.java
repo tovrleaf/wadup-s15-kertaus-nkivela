f0d00bbb9c09e06024afa39158eea8e37bfc1024
Niko Kivelä <niko.kivela@verkkokauppa.com>
2015-10-20 20:15:24 +0300

    New views for courses

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -1,6 +1,7 @@
 package wad.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,8 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DefaultController {
     
     @RequestMapping(method = RequestMethod.GET)
-    public String list() {
+    public String search() {
         
-        return "index";
+        return "search";
+    }
+    
+    @RequestMapping(value = "/course-exams", method = RequestMethod.GET)
+    public String exams(Model model) {
+        model.addAttribute("title", "Course Exams");
+        return "exams";
+    }
+    
+    @RequestMapping(value = "/exams", method = RequestMethod.GET)
+    public String courceExams(Model model) {
+        model.addAttribute("title", "Separate exams");
+        return "exams";
     }
 }

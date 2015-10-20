8246919e4783281e656d69948002c27e6094eef8
Niko Kivelä <niko.kivela@verkkokauppa.com>
2015-10-20 20:36:27 +0300

    Show exams in views by type

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -1,29 +1,36 @@
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.Exam;
+import wad.repository.ExamRepository;
 
 @Controller
 @RequestMapping("*")
 public class DefaultController {
     
+    @Autowired
+    private ExamRepository examRepository;
+    
     @RequestMapping(method = RequestMethod.GET)
     public String search() {
-        
         return "search";
     }
     
     @RequestMapping(value = "/course-exams", method = RequestMethod.GET)
     public String exams(Model model) {
         model.addAttribute("title", "Course Exams");
+        model.addAttribute("courses", examRepository.findByType(Exam.Type.COURSE));
         return "exams";
     }
     
     @RequestMapping(value = "/exams", method = RequestMethod.GET)
     public String courceExams(Model model) {
         model.addAttribute("title", "Separate exams");
+        model.addAttribute("courses", examRepository.findByType(Exam.Type.SEPARATE));
         return "exams";
     }
 }
--- src/main/java/wad/repository/ExamRepository.java
+++ src/main/java/wad/repository/ExamRepository.java
@@ -1,8 +1,10 @@
 package wad.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wad.domain.Exam;
+import wad.domain.Exam.Type;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
-    
+    List<Exam> findByType(Type type);
 }
